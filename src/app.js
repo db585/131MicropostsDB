37e002edb730f17736706df52d61242a41e5bd6d
Denis Irkhin <denis555@me.com>
2018-11-20 21:22:44 -0500

    Progress the proj

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import { http } from './http'
+import { ui } from './ui'
 
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts)
 
 function getPosts () {
   http.get('http://localhost:3008/posts')
-    .then((data) => {
-      console.log(data)
-    })
+    .then(data => ui.showPosts(data))
     .catch(err => console.log(err))
 }
