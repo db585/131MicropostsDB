450f16a369e63342bea172667298e1c06875e1fd
Denis Irkhin <denis555@me.com>
2018-11-21 21:10:26 -0500

    Finish the project

--- src/app.js
+++ src/app.js
@@ -7,6 +7,15 @@ document.addEventListener('DOMContentLoaded', getPosts)
 // Listen for Add post
 document.querySelector('.post-submit').addEventListener('click', submitPost)
 
+// Listen for delete small button
+document.querySelector('#posts').addEventListener('click', deletePost)
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit)
+
+// Listen for cancel of edit state
+document.querySelector('.card-form').addEventListener('click', disableEditState)
+
 // Get posts into UI func
 function getPosts () {
   http.get('http://localhost:3008/posts')
@@ -18,23 +27,96 @@ function getPosts () {
 function submitPost () {
   const title = document.querySelector('#title').value
   const body = document.querySelector('#body').value
+  const hiddId = document.querySelector('#id').value
 
-  const data = {
-    title,
-    body
+  // Validate input fileds
+  if (title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger')
+  } else {
+    const data = {
+      title,
+      body
+    }
+
+    // Check if hidden #id elem is not null than it is an update else it's new post
+    if (hiddId === '') {
+      // Create  new post
+      http.post('http://localhost:3008/posts', data)
+        .then(data => {
+        // Show alert that post has been added
+          ui.showAlert('Post added', 'alert alert-success')
+
+          // Clear input fields
+          ui.clearFields()
+
+          // Update ui
+          getPosts()
+        })
+        .catch(err => console.log(err))
+    } else {
+      // Update post
+      http.put(`http://localhost:3008/posts/${hiddId}`, data)
+        .then(data => {
+          // Show alert that post has been added
+          ui.showAlert('Post updated', 'alert alert-success')
+
+          // Clear input fields
+
+          // Set Add state. Clear input fields [inside changeFormState func]
+          ui.changeFormState('add')
+
+          // Update ui
+          getPosts()
+        })
+        .catch(err => console.log(err))
+    }
   }
+}
 
-  // Create post
-  http.post('http://localhost:3008/posts', data)
-    .then(data => {
-      // Show alert that post has been added
-      ui.showAlert('Post added', 'alert alert-success')
+// Delete post func
+function deletePost (e) {
+  if (e.target.parentElement.classList.contains('delete')) {
+    // console.log(e.target.parentElement.dataset)
+    const id = e.target.parentElement.dataset.id
+    if (window.confirm('Are You sure')) {
+      http.delete(`http://localhost:3008/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', 'alert alert-info')
+          getPosts()
+        })
+        .catch(err => console.log(err))
+    }
+  }
+  e.preventDefault()
+}
 
-      // Clear input fields
-      ui.clearFields()
+// Enable esit post state
+function enableEdit (e) {
+  if (e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent
+    const body = e.target.parentElement.previousElementSibling.textContent
 
-      // Update ui
-      getPosts()
-    })
-    .catch(err => console.log(err))
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill the form with current post
+    ui.fillForm(data)
+  }
+  e.preventDefault()
+}
+
+// Enable add state
+function disableEditState (e) {
+  // console.log('before change state to add    ', ui.formState)
+  // console.log(e.target)
+  // Check for btnCancel click
+  if (e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add')
+    // console.log('after change state to add    ', ui.formState)
+  }
+  e.preventDefault()
 }
--- src/ui.js
+++ src/ui.js
@@ -8,6 +8,7 @@ class UI {
     this.formState = 'add'
   }
 
+  // Show all posts
   showPosts (posts) {
     let output = ''
 
@@ -31,6 +32,7 @@ class UI {
     this.post.innerHTML = output
   }
 
+  // Show alert
   showAlert (msg, classBootstrap) {
     this.clearAlert()
 
@@ -59,6 +61,7 @@ class UI {
     3000)
   }
 
+  // Cleat alert func
   clearAlert () {
     const currentAlert = document.querySelector('.alert')
     if (currentAlert) {
@@ -66,9 +69,78 @@ class UI {
     }
   }
 
+  // Clear fields func
   clearFields () {
     this.titleInput.value = ''
     this.bodyInput.value = ''
+  }
+
+  clearHiddenIdValue () {
+    this.idInput.value = ''
+  }
+
+  // Fill from to edit
+  fillForm (data) {
+    this.titleInput.value = data.title
+    this.bodyInput.value = data.body
+    this.idInput.value = data.id
+
+    // Check the state and change it to "edit"
+    // console.log('before change state to edit   ', ui.formState)
+    if (ui.formState !== 'edit') {
+      this.changeFormState('edit')
+      // console.log('after change state to edit   ', ui.formState)
+    }
+  }
+
+  // Change form state func
+  changeFormState (type) {
+    if (type === 'edit') {
+      // Set text of the button
+      this.postSubmit.textContent = 'Update Post'
+
+      // Set color of the button
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block'
+
+      // Create cancel btn for quiting edit state
+      const btnCancel = document.createElement('button')
+
+      // Add classes to bnt
+      btnCancel.className = 'post-cancel btn btn-light btn-block'
+
+      // Add test to cancel btn
+      btnCancel.appendChild(document.createTextNode('Cancel Edit'))
+
+      // Get the parent
+      const cardForm = document.querySelector('.card-form')
+
+      // Get html element to insert before
+      const formEnd = document.querySelector('.form-end')
+
+      // Insert cancel btn
+      cardForm.insertBefore(btnCancel, formEnd)
+
+      this.formState = 'edit'
+    } else if (type === 'add') {
+      // Set text of the button
+      this.postSubmit.textContent = 'Post It'
+
+      // Set color of the button
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block'
+
+      // Delete btnCancel if it exists
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove()
+
+        // Clear value from hidden #id element
+        this.clearHiddenIdValue()
+
+        // Clear input fields
+        this.clearFields()
+
+        this.formState = 'add'
+      }
+    }
   }
 }
 
